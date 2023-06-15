6b198889f35b98f889b9c52ae40abd8a240b8234
VictorGranado <victorstafussi@gmail.com>
2023-06-14 21:02:13 -0600

    W09 Prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Learning05
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning05 World!");
+        static void Main()
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            shapes.Add(new Square("Red", 5));
+            shapes.Add(new Rectangle("Blue", 4, 6));
+            shapes.Add(new Circle("Green", 3));
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine("Color: " + shape.Color);
+                Console.WriteLine("Area: " + shape.GetArea());
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
     }
 }
+
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,25 @@
+namespace Learning05
+{
+    public class Shape
+    {
+        private string _color;
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public Shape(string color)
+        {
+            _color = color;
+        }
+
+        public virtual double GetArea()
+        {
+            return 0;
+        }
+    }
+}
+
+
